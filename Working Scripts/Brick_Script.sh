457ccaefc6269bf79758d1331e4e3a8ad277f071
Muddassir Nazir <mnstalemate@hotmail.com>
2015-09-22 23:35:06 +0530

    scripts

--- Working Scripts/Brick_Script.sh
+++ Working Scripts/Brick_Script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#lvcreate -L 2G -n brick$1 vg_bricks
-#mkfs.xfs -i size=512 /dev/vg_bricks/brick$1
-#mkdir -p /bricks/brick$1
-#echo "/dev/vg_bricks/brick$1 /bricks/brick$1 xfs defaults 0 0" >> /etc/fstab
-#mount -a
-#df -h
+
+lvcreate -L 2G -n brick$1 vg_bricks`
+mkfs.xfs -i size=512 /dev/vg_bricks/brick$1
+mkdir -p /bricks/brick$1
+echo "/dev/vg_bricks/brick$1 /bricks/brick$1 xfs defaults 0 0" >> /etc/fstab
+mount -a
+df -h
