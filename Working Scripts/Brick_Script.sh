cc8bbadf445d91adcf9bd41e86fcf674ab037d50
Muddassir Nazir <mnstalemate@hotmail.com>
2015-09-23 00:06:50 +0530

    scripts

--- Working Scripts/Brick_Script.sh
+++ Working Scripts/Brick_Script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#Author: Muddassi Nazir
+#This script will create partitions to be used with glusterfs volumes.
+#Use this scripts across multiple machines and create bricks prior to creating volumes.
+#Remember to put a brick number at runtime as ./brick_script.sh 1 or bash brick_script 3 etc. 
 
 lvcreate -L 2G -n brick$1 vg_bricks`
 mkfs.xfs -i size=512 /dev/vg_bricks/brick$1
