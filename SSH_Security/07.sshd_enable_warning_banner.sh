62ec4cf57be00554551ff354e238d3f003eb9c55
Muddassir Nazir <muddassirnazir@users.noreply.github.com>
2015-12-08 20:27:50 +0530

    added more details

--- SSH_Security/07.sshd_enable_warning_banner.sh
+++ SSH_Security/07.sshd_enable_warning_banner.sh
@@ -1,3 +1,8 @@
+#Pre login banner is use for sending a warning message before authentication may be relevant for getting legal protection or just give out information to users. 
+#The contents of the specified file are sent to the remote user before authentication is allowed. 
+#This option is only available for protocol version 2. 
+#By default, no banner is displayed (if you are using latest version of Linux/UNIX then you do not have to worry about version issue).
+
 #To enable the warning banner and ensure it is consistent across the system, add or correct the following line in /etc/ssh/sshd_config:
 
 #Banner /etc/issue
