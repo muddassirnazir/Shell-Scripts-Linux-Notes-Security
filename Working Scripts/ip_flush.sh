732f5120de3b2c142cabd981f5e327a69f8118ba
Muddassir Nazir <muddassirnazir@users.noreply.github.com>
2016-05-01 07:49:20 +0530

    updated details

--- Working Scripts/ip_flush.sh
+++ Working Scripts/ip_flush.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#This script will flush the blocked IP addresses from CSF firewall on a routine basis.
+#This is helpful if you do not have the physical access to your server and you are 
+#getting blocked due to some reason.
+#The drawback is that when the IP addresses are flushed, all the blocked IP 
+#(suspicious IP/ attackers/ brute forcers) are also unblocked.
+#Best solution is to have a static IP so that you can whitelist only that.
+
 echo "Flushing ips from csf.deny"
 /usr/sbin/csf -tf
 /usr/sbin/csf -df
